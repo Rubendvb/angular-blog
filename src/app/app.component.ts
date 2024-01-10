d71905e65f79886915efe9d0ba20de9df738ae75
Rubendvb <rubenvasquez556@gmail.com>
2024-01-10 01:26:42 -0300

    styling title component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, BigCardComponent, MenuTitleComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-blog';
