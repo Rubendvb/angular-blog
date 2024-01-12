951cfa21b8db6d3674288d3a2cfad8c55dc93aff
Rubendvb <rubenvasquez556@gmail.com>
2024-01-12 12:58:41 -0300

    decoupling components

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
   imports: [],
   templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.css'
+  styleUrl: './small-card.component.css',
 })
 export class SmallCardComponent {
+  @Input()
+  photoCover: string =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM179mS-6WFd3NFVpjZzR7LDB3G1jMm_x1CA&usqp=CAU';
 
+  @Input()
+  cardTitle: string = 'Não definido';
 }
