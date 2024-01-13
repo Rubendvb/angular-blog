1d4a5387e3557af4a85c9927a8c404b77e15796a
Rubendvb <rubenvasquez556@gmail.com>
2024-01-13 05:51:35 -0300

    page dynamic

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css',
 })
@@ -14,4 +15,7 @@ export class SmallCardComponent {
 
   @Input()
   cardTitle: string = 'Não definido';
+
+  @Input()
+  Id: string = '0';
 }
