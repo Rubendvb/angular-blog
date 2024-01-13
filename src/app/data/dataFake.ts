9127ed57bb766e93f8766aec560847bf7000db14
Rubendvb <rubenvasquez556@gmail.com>
2024-01-13 02:22:08 -0300

    creating others pages

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    id: 1,
+    photo: 'https://example.com/photo1.jpg',
+    title: 'Curso de Programação em JavaScript',
+    description:
+      'Aprenda os fundamentos do JavaScript e desenvolva suas habilidades de programação.',
+  },
+  {
+    id: 2,
+    photo: 'https://example.com/photo2.jpg',
+    title: 'Guia Completo de Marketing Digital',
+    description:
+      'Descubra estratégias eficazes de marketing digital para impulsionar seus negócios online.',
+  },
+  {
+    id: 3,
+    photo: 'https://example.com/photo3.jpg',
+    title: 'Yoga para Iniciantes',
+    description:
+      'Explore as práticas de yoga para melhorar sua flexibilidade, equilíbrio e bem-estar geral.',
+  },
+  {
+    id: 4,
+    photo: 'https://example.com/photo4.jpg',
+    title: 'Receitas Gourmet para Amadores',
+    description:
+      'Aprenda a preparar pratos gourmet impressionantes, mesmo sendo um amante da culinária amadora.',
+  },
+];
